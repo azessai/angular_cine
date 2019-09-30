887ecdcbd1fa6e67b8a436b31216ffe839f086dd
azessai <azeddine.essai@gmail.com>
2019-09-30 13:12:57 +0200

    info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieInfoComponent } from './movie-info/movie-info.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'info/:movieId', component: MovieInfoComponent },
+  {path: '**', component: MovieListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MoviePreviewComponent } from './movie-preview/movie-preview.component';
+import { MovieInfoComponent } from './movie-info/movie-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    MoviePreviewComponent,
+    MovieInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
