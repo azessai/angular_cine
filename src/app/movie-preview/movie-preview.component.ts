56d840d37e3b74a7c445911873229c4f328a4107
azessai <azeddine.essai@gmail.com>
2019-10-20 23:31:45 +0200

    stars

--- src/app/movie-preview/movie-preview.component.ts
+++ src/app/movie-preview/movie-preview.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'ca-movie-preview',
@@ -13,13 +14,30 @@ import { MatCardModule } from '@angular/material/card';
 export class MoviePreviewComponent implements OnInit {
 
   @Input() movie: Movie;
+  starCount = 10;
+  ratingArr = [];
+  color = 'accent';
+
   constructor() { }
 
   ngOnInit() {
+    for (let index = 0; index < this.starCount; index++) {
+      this.ratingArr.push(index);
+    }
   }
-
+  showIcon(index: number) {
+    if (this.movie.vote_average >= index + 1) {
+      return 'star';
+    } else {
+      return 'star_border';
+    }
+  }
 }
-
+export enum StarRatingColor {
+  primary = 'primary',
+  accent = 'accent',
+  warn = 'warn'
+}
 @NgModule({
   declarations: [
     MoviePreviewComponent
@@ -28,7 +46,8 @@ export class MoviePreviewComponent implements OnInit {
     CommonModule,
     MatButtonModule,
     RouterModule,
-    MatCardModule
+    MatCardModule,
+    MatIconModule
   ],
   exports: [
     MoviePreviewComponent
